bb07c3aa214100b914eb2565e2ea9238e9a6766f
Inès SLOTWINSKI <ines.slotwinski@Digital-Grenoble.local>
2022-11-14 11:15:55 +0100

    add crud

---
+++ app/Models/ChoiceTshirt.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChoiceTshirt extends Model
+{
+    use HasFactory;
+}
